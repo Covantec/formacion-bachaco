feb3006b5454690d9f904975ed966ebfb4857c52
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-06-27 01:43:03 -0400

    Excluir directorio de virtualenv

--- conf.py
+++ conf.py
@@ -73,7 +73,7 @@ today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'README.rst']
+exclude_patterns = ['_build', 'README.rst', 'venv']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
