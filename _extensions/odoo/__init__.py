b82faff66b1efbaab8838159f1ade1358a951e09
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-06-28 10:13:05 -0400

    Trata de solventar error AttributeError: 'NoneType' object has no attribute 'setdefault'

--- _extensions/odoo/__init__.py
+++ _extensions/odoo/__init__.py
@@ -19,7 +19,11 @@ def setup(app):
     app.connect('html-page-context', update_meta)
 
 def update_meta(app, pagename, templatename, context, doctree):
-    meta = context.setdefault('meta', {})
+    # meta = context.setdefault('meta', {})
+    # meta.setdefault('banner', app.config.odoo_cover_default)
+    meta = context.get('meta')
+    if meta is None:
+        meta = context['meta'] = {}
     meta.setdefault('banner', app.config.odoo_cover_default)
 
 def navbarify(node, navbar=None):
